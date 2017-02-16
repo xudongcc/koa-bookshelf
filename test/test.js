04c47b98dac84ed997a5175f0c9e55d8f9cd3c43
xudong <i@xudong.cc>
2017-02-16 21:07:31 +0800

    add README.md

--- test/test.js
+++ test/test.js
@@ -6,13 +6,12 @@ const app = new Koa();
 app.use(bookshelf('/test/models', {
   client: 'sqlite3',
   connection: {
-    filename: "./test.db"
+    filename: './test.db'
   }
 }));
 
 app.use(async function (ctx, next) {
   const User = ctx.models.User;
-  const Post = ctx.models.Post;
   var users = await User.where({}).fetchAll();
 
   ctx.body = users;
